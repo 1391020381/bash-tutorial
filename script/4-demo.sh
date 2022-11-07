02995285da12e6cc08bf6fe0ee00958e79c72e43
1391020381 <1391020381@qq.com>
2022-11-07 15:15:20 +0800

    Bash 基本语法 变量 运算 判断循环 函数 其他

--- script/4-demo.sh
+++ script/4-demo.sh
@@ -4,4 +4,15 @@ number=0
 while [ "$number" -lt 10 ];do
     echo "Number = $number"
     number=$((number+1))
+done   
+
+
+for i in word1 word2 word3;do
+    echo $i
 done    
+
+
+select brand in Samsung Sony iphone symphony Walton
+do
+    echo "You have chosen $brand"
+done
